849c30254cc986915a6e2501fc550a5ad5b532a2
Tristan Delaigue <tristan.del44@gmail.com>
2024-10-08 09:22:07 +0200

    Another commit before puuling

--- obj/Debug/net8.0/SeleniaCity.AssemblyInfo.cs
+++ obj/Debug/net8.0/SeleniaCity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SeleniaCity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce21e2c93604a47931ae947445a335310c471acd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27af5fe5c811de51aa7594ef92a13a846cef9ad0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SeleniaCity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SeleniaCity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
